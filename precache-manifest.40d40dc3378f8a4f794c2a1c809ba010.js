ef55999c82eaad1dbc20148506a59442b9c7e542
Marcin Taff <marcin.taff@gmail.com>
2023-10-18 21:34:08 +0200

    Updates

---
+++ precache-manifest.40d40dc3378f8a4f794c2a1c809ba010.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8b0b582877eaf578078a5c4a79e5668",
+    "url": "/gramy-to-react-website/index.html"
+  },
+  {
+    "revision": "9fe7ffa53abc70096b21",
+    "url": "/gramy-to-react-website/static/css/2.80f89dfb.chunk.css"
+  },
+  {
+    "revision": "062985fc5b5077d1fc4b",
+    "url": "/gramy-to-react-website/static/css/main.2dd7c1b3.chunk.css"
+  },
+  {
+    "revision": "9fe7ffa53abc70096b21",
+    "url": "/gramy-to-react-website/static/js/2.8543a35f.chunk.js"
+  },
+  {
+    "revision": "f7d5cf260f8ea9504d579b89b79a931e",
+    "url": "/gramy-to-react-website/static/js/2.8543a35f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "062985fc5b5077d1fc4b",
+    "url": "/gramy-to-react-website/static/js/main.76152964.chunk.js"
+  },
+  {
+    "revision": "f116b12291ded116f337",
+    "url": "/gramy-to-react-website/static/js/runtime-main.787042e9.js"
+  },
+  {
+    "revision": "810c4f4080cb27af8dfd967a9b18c285",
+    "url": "/gramy-to-react-website/static/media/1.810c4f40.jpg"
+  },
+  {
+    "revision": "2ecc8f2ea9ec513bb3cf74a31c7c59cb",
+    "url": "/gramy-to-react-website/static/media/10.2ecc8f2e.jpg"
+  },
+  {
+    "revision": "42bbdf5c84c892d3dd7d27604d24bdb6",
+    "url": "/gramy-to-react-website/static/media/11.42bbdf5c.jpg"
+  },
+  {
+    "revision": "e33d5ec65a4408a111a9ae1abd78284a",
+    "url": "/gramy-to-react-website/static/media/12.e33d5ec6.jpg"
+  },
+  {
+    "revision": "17dde95b1564a3efe17abdf753db02cf",
+    "url": "/gramy-to-react-website/static/media/13.17dde95b.jpg"
+  },
+  {
+    "revision": "e085c1ea2a3e082bf2b96b95ef912436",
+    "url": "/gramy-to-react-website/static/media/14.e085c1ea.jpg"
+  },
+  {
+    "revision": "48ca576b745949f4b65c0dfe945912b1",
+    "url": "/gramy-to-react-website/static/media/15.48ca576b.jpg"
+  },
+  {
+    "revision": "96a708ad83e9cf2d7dc4ee4a6d54aef0",
+    "url": "/gramy-to-react-website/static/media/16.96a708ad.jpg"
+  },
+  {
+    "revision": "36061d65b7603d9906049d73d6748b6c",
+    "url": "/gramy-to-react-website/static/media/17.36061d65.jpg"
+  },
+  {
+    "revision": "0277e9ea31377e797c4857c5a1f96eb9",
+    "url": "/gramy-to-react-website/static/media/18.0277e9ea.jpg"
+  },
+  {
+    "revision": "eec8113018a581eba86d32de5bbf46f6",
+    "url": "/gramy-to-react-website/static/media/19.eec81130.jpg"
+  },
+  {
+    "revision": "8a0c12847ed1535f28b6f753d6fa578c",
+    "url": "/gramy-to-react-website/static/media/2.8a0c1284.jpg"
+  },
+  {
+    "revision": "f96b9761a0e527b64408a5f096186a86",
+    "url": "/gramy-to-react-website/static/media/20.f96b9761.jpg"
+  },
+  {
+    "revision": "e17a8573739805006580a28f167d7c08",
+    "url": "/gramy-to-react-website/static/media/21.e17a8573.jpg"
+  },
+  {
+    "revision": "f8edbc4b3ad3ad75d91be41d346f8352",
+    "url": "/gramy-to-react-website/static/media/22.f8edbc4b.jpg"
+  },
+  {
+    "revision": "d31202094adce8bff3a9b955e08cc2a7",
+    "url": "/gramy-to-react-website/static/media/23.d3120209.jpg"
+  },
+  {
+    "revision": "cd13f0fdcf21c0cc0d5e5b3beb93006c",
+    "url": "/gramy-to-react-website/static/media/24.cd13f0fd.jpg"
+  },
+  {
+    "revision": "247d0ac52d6a4006bb752cd4769b9a4b",
+    "url": "/gramy-to-react-website/static/media/25.247d0ac5.jpg"
+  },
+  {
+    "revision": "dddc70fe9589bf8c0f09a23feb1c730b",
+    "url": "/gramy-to-react-website/static/media/26.dddc70fe.jpg"
+  },
+  {
+    "revision": "32de41d4a5d0d8d24bab55ee5b796b1a",
+    "url": "/gramy-to-react-website/static/media/27.32de41d4.jpg"
+  },
+  {
+    "revision": "48ab95731813cc9ddafe638b507b255f",
+    "url": "/gramy-to-react-website/static/media/28.48ab9573.jpg"
+  },
+  {
+    "revision": "217882e21bf66b22d57fdcd1de22305c",
+    "url": "/gramy-to-react-website/static/media/29.217882e2.jpg"
+  },
+  {
+    "revision": "facbf2124980ddef2a90e0d1de7577fb",
+    "url": "/gramy-to-react-website/static/media/3.facbf212.jpg"
+  },
+  {
+    "revision": "17de6861e2c44386e089df2aa2eb886a",
+    "url": "/gramy-to-react-website/static/media/30.17de6861.jpg"
+  },
+  {
+    "revision": "7e85c0977aa15893cad1ba20bc690f10",
+    "url": "/gramy-to-react-website/static/media/31.7e85c097.jpg"
+  },
+  {
+    "revision": "63bbc369c8f6a503658a411bc6de8bf9",
+    "url": "/gramy-to-react-website/static/media/32.63bbc369.jpg"
+  },
+  {
+    "revision": "9d2890a97176bb5763a3ebeb4259e525",
+    "url": "/gramy-to-react-website/static/media/33.9d2890a9.jpg"
+  },
+  {
+    "revision": "82a92248070c0112b05996b73d5f867e",
+    "url": "/gramy-to-react-website/static/media/34.82a92248.jpg"
+  },
+  {
+    "revision": "ed7a6870b3129d4a5dc88ef9753044e0",
+    "url": "/gramy-to-react-website/static/media/35.ed7a6870.jpg"
+  },
+  {
+    "revision": "e40b391f885dc83d812001b6bb086cf9",
+    "url": "/gramy-to-react-website/static/media/36.e40b391f.jpg"
+  },
+  {
+    "revision": "12533a21242e6e86c7f0e836ee8c7218",
+    "url": "/gramy-to-react-website/static/media/37.12533a21.jpg"
+  },
+  {
+    "revision": "ee202ad7720ace387bc898d52659c486",
+    "url": "/gramy-to-react-website/static/media/4.ee202ad7.jpg"
+  },
+  {
+    "revision": "d35cc811551bc162d4ae35b6d385adfd",
+    "url": "/gramy-to-react-website/static/media/5.d35cc811.jpg"
+  },
+  {
+    "revision": "a07f51254178bc92a361eeb658ca41bd",
+    "url": "/gramy-to-react-website/static/media/6.a07f5125.jpg"
+  },
+  {
+    "revision": "ab676d9fbe36bb3ee5550d2fbb595bbc",
+    "url": "/gramy-to-react-website/static/media/7.ab676d9f.jpg"
+  },
+  {
+    "revision": "822bfcf2fe0f3cde5f81c296b351c51f",
+    "url": "/gramy-to-react-website/static/media/8.822bfcf2.jpg"
+  },
+  {
+    "revision": "188eb281d0131dfad0a07df47fe9c866",
+    "url": "/gramy-to-react-website/static/media/9.188eb281.jpg"
+  },
+  {
+    "revision": "4f661692ec09642a3be6041c653ae7e3",
+    "url": "/gramy-to-react-website/static/media/Dawid.4f661692.png"
+  },
+  {
+    "revision": "fddb706ab7ac300822e3b8262b854544",
+    "url": "/gramy-to-react-website/static/media/Marcin.fddb706a.png"
+  },
+  {
+    "revision": "61511a6194926708119064be04dd3f0f",
+    "url": "/gramy-to-react-website/static/media/Mariusz.61511a61.png"
+  },
+  {
+    "revision": "310b3400453cecb6ea75d5ec4a081e7f",
+    "url": "/gramy-to-react-website/static/media/Michal.310b3400.png"
+  },
+  {
+    "revision": "2b7349e71d7888460a6ff0f8b34a1ee1",
+    "url": "/gramy-to-react-website/static/media/Ola.2b7349e7.png"
+  },
+  {
+    "revision": "d3e005b2fe21d2600febc44238b1f432",
+    "url": "/gramy-to-react-website/static/media/Robert.d3e005b2.png"
+  },
+  {
+    "revision": "f47b5a62c1850415c3eb31c1f59b86d8",
+    "url": "/gramy-to-react-website/static/media/blob.f47b5a62.svg"
+  },
+  {
+    "revision": "af73b9b7eb71cc90d7c16d9a38f550c1",
+    "url": "/gramy-to-react-website/static/media/gt-landing-transparent.af73b9b7.png"
+  },
+  {
+    "revision": "1521fa786c2d17fc8b0179a69458984e",
+    "url": "/gramy-to-react-website/static/media/logo.1521fa78.svg"
+  },
+  {
+    "revision": "b0a4843bd81e01878f2cf02984703da1",
+    "url": "/gramy-to-react-website/static/media/waves.b0a4843b.svg"
+  }
+]);
